b3bfcf341f2b8a41c82e88347805ed84de740404
jatin foujdar <jatin321001@gmail.com>
2025-02-28 14:42:35 +0530

    fetch ui commit

---
+++ Fetch-API-IOS/View/FetchView.swift
@@ -0,0 +1,18 @@
+//
+//  FetchView.swift
+//  Fetch-API-IOS
+//
+//  Created by jatin foujdar on 28/02/25.
+//
+
+import SwiftUI
+
+struct FetchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FetchView()
+}
---
+++ Fetch-API-IOS/ViewModel/NetworkViewModel.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+
+
+class NetworkManager{
+    
+    private let apiUrl = "https://dummyjson.com/users"
+    
+    func ApiFetch() async throws -> [User]{
+        guard let url = URL(string: apiUrl) else{
+            throw URLError(.badURL)
+        }
+        
+        var req = URLRequest(url: url)
+        req.httpMethod = "GET"
+        
+        
+        let (data, response) = try await URLSession.shared.data(for: req)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
+        let decoder = JSONDecoder()
+        
+        do{
+            let userResponse =  try decoder.decode(UserResponse.self, from: data)
+            return userResponse.users
+        }catch{
+            throw error
+        }
+    }
+}
+
+
+
