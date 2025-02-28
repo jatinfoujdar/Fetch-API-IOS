04bee69d78868885833ac3f23f944d4200e95801
jatin foujdar <jatin321001@gmail.com>
2025-02-28 13:31:42 +0530

    Initial Commit

---
+++ Fetch-API-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fetch-API-IOS
+//
+//  Created by jatin foujdar on 28/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fetch-API-IOS/Fetch_API_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_API_IOSApp.swift
+//  Fetch-API-IOS
+//
+//  Created by jatin foujdar on 28/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_API_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
